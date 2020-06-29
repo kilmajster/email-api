a2e03554ae5011fa1f9753a0aea9a37271332b70
jvmops <jvmops@gmail.com>
2020-06-30 00:31:42 +0200

    jacoco added - API specification = 0.3 coverage

--- src/main/java/com/jvmops/api/emails/model/EmailMessageDto.java
+++ src/main/java/com/jvmops/api/emails/model/EmailMessageDto.java
@@ -6,7 +6,6 @@ import org.bson.types.ObjectId;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
