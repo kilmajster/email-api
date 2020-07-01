a895d31d1bc11d04c9d50ca4f62e3768ab2fc697
jvmops <jvmops@gmail.com>
2020-07-01 02:50:26 +0200

    sender :: Index added to the status of an email message

--- src/main/java/com/jvmops/api/emails/model/EmailMessage.java
+++ src/main/java/com/jvmops/api/emails/model/EmailMessage.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Set;
@@ -19,6 +20,7 @@ public class EmailMessage {
     Set<String> recipients;
     String topic;
     String body;
+    @Indexed
     Status status;
     Priority priority;
     @Version
